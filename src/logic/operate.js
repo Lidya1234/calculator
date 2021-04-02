a1abd1145c40305cac4489a69ddba521d56e22dd
Lidya1234 <lidya94gher@gmail.com>
2021-04-02 19:13:34 +0300

    Change the signs to big number methods

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,23 +2,23 @@ import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
   let result;
-  const number1 = Big(+numberOne);
-  const number2 = Big(+numberTwo);
+  const number1 = Big(numberOne);
+  const number2 = Big(numberTwo);
   switch (operation) {
     case '+':
-      result = Big(number1 + number2);
+      result = Big(number1.plus(number2));
       break;
     case '-':
-      result = Big(number1 - number2);
+      result = Big(number1.minus(number2));
       break;
     case 'X':
-      result = Big(number1 * number2);
+      result = Big(number1.times(number2));
       break;
     case '÷':
-      result = Big(number1 / number2);
+      result = Big(number1.div(number2));
       break;
     case '%':
-      result = `${Big(number1 / 100)}%`;
+      result = `${Big(number1.div(100))}%`;
       break;
     default:
       result = '0';
