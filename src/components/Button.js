5966dc6fd56451952dfb05c38fa0e0f9dc2c5293
Lidya1234 <lidya94gher@gmail.com>
2021-04-02 18:13:40 +0300

    Add clickHandler   src/components/Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,13 @@
 import { PropTypes } from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
-const Buttons = ({ buttonName }) => (
-  <Button className="btn rounded-0 keys">{buttonName}</Button>
+const Buttons = ({ buttonName, clickHandler }) => (
+  <Button className="btn rounded-0 keys" onClick={() => clickHandler({ buttonName })}>{buttonName}</Button>
 );
 
 Buttons.propTypes = {
   buttonName: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Buttons;
