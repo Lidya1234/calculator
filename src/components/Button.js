955a631b619e8819a457b1ccbc819b8376d6876f
Lidya1234 <lidya94gher@gmail.com>
2021-04-07 19:11:46 +0300

    Add color prob to button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,14 @@
 import { PropTypes } from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
-const Buttons = ({ buttonName, clickHandler }) => (
-  <Button className="btn rounded-0 keys" onClick={() => clickHandler({ buttonName })}>{buttonName}</Button>
+const Buttons = ({ buttonName, color, clickHandler }) => (
+  <Button className={`btn rounded-0 keys btn-${color}`} onClick={() => clickHandler({ buttonName })}>{buttonName}</Button>
 );
 
 Buttons.propTypes = {
   buttonName: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string.isRequired,
 };
 
 export default Buttons;
