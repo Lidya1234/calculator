c93fd06b4d959ed84935ff1e03e244c358004cea
Lidya1234 <lidya94gher@gmail.com>
2021-04-07 19:12:49 +0300

    Add color prop with value to  src/components/ButtonPanel.js

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,37 +1,40 @@
 import { PropTypes } from 'prop-types';
 import Buttons from './Button';
+import * as styles from '../style.module.css';
 
 const ButtonPanel = ({ clickHandler }) => (
   <>
-    <div>
-      <Buttons buttonName="AC" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="+/-" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="%" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="÷" clickHandler={(buttonName) => clickHandler(buttonName)} />
-    </div>
-    <div>
-      <Buttons buttonName="7" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="8" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="9" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="X" clickHandler={(buttonName) => clickHandler(buttonName)} />
-    </div>
-    <div>
-      <Buttons buttonName="4" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="5" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="6" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="-" clickHandler={(buttonName) => clickHandler(buttonName)} />
-    </div>
-    <div>
-      <Buttons buttonName="1" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="2" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="3" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="+" clickHandler={(buttonName) => clickHandler(buttonName)} />
-    </div>
-    <div>
-      <Buttons buttonName="0" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="." clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName="=" clickHandler={(buttonName) => clickHandler(buttonName)} />
-      <Buttons buttonName=" " clickHandler={(buttonName) => clickHandler(buttonName)} />
+    <div className={styles.buttonpannel}>
+      <div>
+        <Buttons buttonName="AC" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="+/-" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="%" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="÷" color="warning" clickHandler={(buttonName) => clickHandler(buttonName)} />
+      </div>
+      <div>
+        <Buttons buttonName="7" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="8" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="9" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="X" color="warning" clickHandler={(buttonName) => clickHandler(buttonName)} />
+      </div>
+      <div>
+        <Buttons buttonName="4" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="5" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="6" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="-" color="warning" clickHandler={(buttonName) => clickHandler(buttonName)} />
+      </div>
+      <div>
+        <Buttons buttonName="1" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="2" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="3" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="+" color="warning" clickHandler={(buttonName) => clickHandler(buttonName)} />
+      </div>
+      <div>
+        <Buttons buttonName="0" color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="." color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName=" " color="light" clickHandler={(buttonName) => clickHandler(buttonName)} />
+        <Buttons buttonName="=" color="warning" clickHandler={(buttonName) => clickHandler(buttonName)} />
+      </div>
     </div>
   </>
 
