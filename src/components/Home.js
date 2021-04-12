c058765a04243f3423da793c403cdd6b69378655
Lidya1234 <lidya94gher@gmail.com>
2021-04-12 17:59:17 +0300

    Add home componenet

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import * as styles from '../style.module.css';
+
+const Home = () => (
+  <div className={styles.home}>
+    <p>
+      Put life in perspective with some short
+      yet sage pieces of advice.
+      These wise and beautiful words from
+      your favorite positive thinkers
+      will get you in the right mindset to
+      tackle whatever obstacles lie ahead.
+      Inspirational quotes from successful
+      people like Oprah, Helen Keller, and Maya Angelou can
+      help you reframe negative thoughts during tough times
+      and change your attitude so you feel
+      refreshed and motivated.
+
+    </p>
+  </div>
+);
+
+export default Home;
