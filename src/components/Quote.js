415927f6a20bfd7fae1bafd8f7928eec72cbe1f7
Lidya1234 <lidya94gher@gmail.com>
2021-04-12 18:01:04 +0300

    Add src/components/Quote.js

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import * as styles from '../style.module.css';
+
+const Quote = () => (
+  <div className={styles.quote}>
+    <p>
+
+      Mathematics is not about numbers, equations,
+      computations, or algorithms: it is about understanding.
+      –William Paul ThurstonHomeCalculatorQuote
+
+    </p>
+  </div>
+);
+
+export default Quote;
